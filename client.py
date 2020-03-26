c6925c8765a963000d0d214a3742e320b861f9de
Damian Zalewski <damian.zalewski34@gmail.com>
2020-03-26 22:38:22 +0100

    Added log/server.log which contains all stuff received by server

--- client.py
+++ client.py
@@ -5,44 +5,54 @@ from _datetime import datetime
 import time
 from threading import Thread
 
-HEADER_LENGTH = 40
+# initial variables
+HEADER_LENGTH = 10
 IP = '127.0.0.1'
 PORT = 1234
-
-my_username = input("Your username: ")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP,PORT))
 client_socket.setblocking(False)
 
+# declaring username
+my_username = input("Your username: ")
 
+# sending username to server
 username = my_username.encode('utf-8')
 username_header = f'{len(username):<{HEADER_LENGTH}}'.encode('utf-8')
 client_socket.send(username_header+username)
 
+
+# successful connection
 while True:
     date_time_stamp = datetime.fromtimestamp(time.time())
     message = input(f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: {my_username} > ")
 
+    # if message is written - print it for client and send to server
     if message:
         message = message.encode('utf-8')
         message_header = f'{len(message) :< {HEADER_LENGTH}}'.encode('utf-8')
         client_socket.send(message_header + message)
     try:
-        while True: # receive things
+        # receive messages
+        while True:
             username_header = client_socket.recv(HEADER_LENGTH)
             if not len(username_header):
                 print("Connection closed by the server")
                 sys.exit()
 
+            # decoding message
             username_length = int(username_header.decode('utf-8').strip())
             username = client_socket.recv(username_length).decode('utf-8')
-
             message_header = client_socket.recv(HEADER_LENGTH)
             message_length = int(message_header.decode('utf-8').strip())
             message = client_socket.recv(message_length).decode('utf-8')
+
+            # printing message with system time from client [should be from server - working on it]
             date_time_stamp = datetime.fromtimestamp(time.time())
             print(f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: {username} > {message}")
 
+
+# handling exceptions
     except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print('Reading error', str(e))
--- server.py
+++ server.py
@@ -2,63 +2,78 @@ import socket
 import select
 from _datetime import datetime
 import time
+import csv
 
-HEADER_LENGTH = 40
+# initial variables
+HEADER_LENGTH = 10
 IP = '127.0.0.1'
 PORT = 1234
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind((IP,PORT))
+server_socket.bind((IP, PORT))
 server_socket.listen()
-
 sockets_list = [server_socket]
-
 clients = {}
 
 
+# handles message receiving
 def receive_message(client_socket):
     try:
         message_header = client_socket.recv(HEADER_LENGTH)
-        if not len(message_header):
+        if not len(message_header): # user disconnected
             return False
         message_length = int(message_header.decode("utf-8").strip())
         return {"header": message_header, "data": client_socket.recv(message_length)}
     except:
         return False
 
+
 while True:
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-    for notified_socket in read_sockets:
+    for notified_socket in read_sockets: # got a new connection
         if notified_socket == server_socket:
+            # getting unique client socket and address
             client_socket, client_address = server_socket.accept()
-
             user = receive_message(client_socket)
-            if user is False:
+            if user is False :
                 continue
 
             sockets_list.append(client_socket)
+            # saving username to csv with flag and receiving info about connection
             clients[client_socket] = user
             date_time_stamp = datetime.fromtimestamp(time.time())
-            print(f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}")
+            log = open(f"log\server{date_time_stamp.strftime('%Y-%m-%d')}.log", 'a')
+            print(
+                f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}",
+                file=log)
+            log.close()
 
-        else:
+        else: # if notified socket is not a server socket - means message
             message = receive_message(notified_socket)
 
-            if message is False:
+            if message is False: # if client disconnects message would be empty
                 date_time_stamp = datetime.fromtimestamp(time.time())
-                print(f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: Closed connection for {clients[notified_socket]['data'].decode('utf-8')}")
+                log = open(f"log\server{date_time_stamp.strftime('%Y-%m-%d')}.log", 'a')
+                print(
+                    f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: Closed connection for {clients[notified_socket]['data'].decode('utf-8')}",
+                    file=log)
+                log.close()
                 sockets_list.remove(notified_socket)
                 del clients[notified_socket]
                 continue
+            # message is received - print to server.log info about it
             user = clients[notified_socket]
-            print(f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+            log = open(f"log\server{date_time_stamp.strftime('%Y-%m-%d')}.log", 'a')
+            print(
+                f"[{date_time_stamp.strftime('%Y-%m-%d %H:%M:%S')}]: Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}",
+                file=log)
+            log.close()
 
+            # sending message to all clients
             for client_socket in clients:
                 if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message ['header'] + message['data'])
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
 
     for notified_socket in exception_sockets:
         sockets_list.remove(notified_socket)
